4a48a6927a25bef6907d25a80b50df880ae8b42b
truposchet <art.shaper1@gmail.com>
2018-03-07 22:30:32 +0300

    Minor commit

---
+++ app/src/main/java/com/example/tom/criminalintent/CrimeListActivity.java
@@ -0,0 +1,14 @@
+package com.example.tom.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Tom on 05.03.2018.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
---
+++ app/src/main/java/com/example/tom/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.example.tom.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Tom on 05.03.2018.
+ */
+
+public class CrimeListFragment extends Fragment {
+}
