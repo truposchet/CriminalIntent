767d837c0ccd03ff3d972d5058cb3a651e0552a7
truposchet <art.shaper1@gmail.com>
2018-04-04 13:53:05 +0300

    ConstrainLayout

--- app/src/main/java/com/example/tom/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/tom/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.example.tom.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -60,9 +61,8 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(getActivity(),
-                    mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT)
-                    .show();
+           Intent intent = new Intent(getActivity(), CrimeActivity.class);
+           startActivity(intent);
         }
     }
     private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
