7cdb4afc96ac66f2ab3d2e6dec02b233b834bd68
truposchet <art.shaper1@gmail.com>
2018-04-20 23:27:10 +0300

    ViewPager practice started

--- app/src/main/java/com/example/tom/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/example/tom/criminalintent/CrimePagerActivity.java
@@ -1,12 +1,36 @@
 package com.example.tom.criminalintent;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.List;
+
 public class CrimePagerActivity extends AppCompatActivity {
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime_pager);
+        mViewPager = (ViewPager) findViewById(R.id.crime_view_pager);
+        mCrimes = CrimeLab.get(this).getCrimes();
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
     }
 }
